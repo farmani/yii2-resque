fa1179e4f191a78b89dd23186fdfec98c086c99f
farmani <ramin.farmani@gmail.com>
2015-10-02 19:02:11 +0330

    resolve redise issue

--- lib/Resque/Redis.php
+++ lib/Resque/Redis.php
@@ -1,29 +1,28 @@
 <?php
 namespace resque\lib\Resque;
-use resque\lib\Credis\Credis_Client;
 class RedisApi extends \Redis
 {
-	private static $defaultNamespace = 'resque:';
+    private static $defaultNamespace = 'resque:';
 
-	public $password = null;
+    public $password = null;
 
-	public function __construct($host, $database = 0, $password = null, $timeout = 60)
-	{
-		parent::__construct();
+    public function __construct($host, $database = 0, $password = null, $timeout = 60)
+    {
+        parent::__construct();
 
-		$server = explode(':', $host);
+        $server = explode(':', $host);
 
-		$this->host = $server[0];
-		$this->port = $server[1];
-		$this->password = $password;
-		$this->timeout = $timeout;
+        $this->host = $server[0];
+        $this->port = $server[1];
+        $this->password = $password;
+        $this->timeout = $timeout;
 
-		$this->establishConnection();
-	}
+        $this->establishConnection();
+    }
 
-	function establishConnection()
-	{
-		$this->connect($this->host, (int) $this->port, (int) $this->timeout);
+    function establishConnection()
+    {
+        $this->connect($this->host, (int) $this->port, (int) $this->timeout);
 
         if (isset($this->password) && !empty($this->password)) {
             if ($this->auth($this->password) === false) {
@@ -31,19 +30,19 @@ class RedisApi extends \Redis
             }
         }
 
-		$this->setOption(\Redis::OPT_PREFIX, self::$defaultNamespace);
-	}
+        $this->setOption(\Redis::OPT_PREFIX, self::$defaultNamespace);
+    }
 
-	public function prefix($namespace)
-	{
-		if (empty($namespace)) $namespace = self::$defaultNamespace;
-		if (strpos($namespace, ':') === false) {
-			$namespace .= ':';
-		}
-		self::$defaultNamespace = $namespace;
-                      
-		$this->setOption(\Redis::OPT_PREFIX, self::$defaultNamespace);
-	}
+    public function prefix($namespace)
+    {
+        if (empty($namespace)) $namespace = self::$defaultNamespace;
+        if (strpos($namespace, ':') === false) {
+            $namespace .= ':';
+        }
+        self::$defaultNamespace = $namespace;
+
+        $this->setOption(\Redis::OPT_PREFIX, self::$defaultNamespace);
+    }
 }
 
 class Redis extends RedisApi {}
